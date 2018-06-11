2ff4d3fea01d642d6e2f1fa34a2a721aff523588
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-11 21:20:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,34 @@
 def starts_with_a_vowel?(word)
-
+matching = word.match(/^[AEIOUaeiou]\w+/)
+if matching
+  true
+else 
+  false 
+end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+uning = text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+fiveletterword = text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+first_word_cap = text.match(/^[A-Z].+[\.!?]$/)
+if first_word_cap
+  true
+else
+  false
+end 
 end
 
 def valid_phone_number?(phone)
-
+phone_number = phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
+if phone_number
+  true
+else
+  false
+end 
 end
